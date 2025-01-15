62d97f8f54d6b318f152e3c63f365146dd16d51b
Nguyen--Kerbel Axel <axel.nguyen-kerbel@student-cs.fr>
2025-01-15 23:40:08 +0100

    Delete additional packages - implement new classes in pre-existing ones

--- lavague-core/lavague/core/__init__.py
+++ lavague-core/lavague/core/__init__.py
@@ -6,10 +6,11 @@ from lavague.core.extractors import (
 )
 from lavague.core.retrievers import OpsmSplitRetriever
 from lavague.core.world_model import WorldModel
-from lavague.core.value_function import ValueFunction
+from lavague.core.world_model import ValueFunction
 from lavague.core.utilities.version_checker import check_latest_version
 from lavague.core.action_engine import ActionEngine
 from lavague.core.agents import WebAgent
+from lavague.core.agents import TreeAgent
 
 import os
 import warnings
