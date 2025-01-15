c3ea0b09a741bd858a92bdd6ed5c7ee14a55dc94
Nguyen--Kerbel Axel <axel.nguyen-kerbel@student-cs.fr>
2025-01-15 18:12:49 +0100

    __init__.py modification for package value_function.py

--- lavague-core/lavague/core/__init__.py
+++ lavague-core/lavague/core/__init__.py
@@ -6,6 +6,7 @@ from lavague.core.extractors import (
 )
 from lavague.core.retrievers import OpsmSplitRetriever
 from lavague.core.world_model import WorldModel
+from lavague.core.value_function import ValueFunction
 from lavague.core.utilities.version_checker import check_latest_version
 from lavague.core.action_engine import ActionEngine
 from lavague.core.agents import WebAgent
